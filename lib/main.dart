c270b53266a64b7cd970ea6077f5268c2b4e78bb
Mahmoud Youssef <mahmoudyousse220@gmail.com>
2024-11-29 15:45:58 +0200

    App integrated with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const CoffeShopApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class CoffeShopApp extends StatelessWidget {
+  const CoffeShopApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter  Demo',
-      theme: ThemeData(),
-      home: const Scaffold(
-    body: Text("hello "),
-    )
-    );
-  }}
+        debugShowCheckedModeBanner: false,
+         home: SafeArea(
+          child: Scaffold(
+            backgroundColor: Colors.black,
+              body: Image.asset(
+            'assets/Images/Image Onboarding.png',
+          )),
+        ));
+  }
+}
