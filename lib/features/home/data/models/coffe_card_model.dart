1933320107f0befe85f96d06aedb6898faeb2539
Mahmoud-abdelsama <m.m.abdelsamad82@gmail.com>
2024-11-30 19:57:40 +0200

    create caffe cart model

---
+++ lib/features/home/data/models/coffe_card_model.dart
@@ -0,0 +1,10 @@
+class CoffeCardModel{
+
+  final String name;
+  final String image;
+  final String description;
+  final double  price;
+  final double rate;
+
+  CoffeCardModel({required this.name,required this.image,required this.description,required this.price,required this.rate});
+}
